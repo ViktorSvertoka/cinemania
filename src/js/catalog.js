7208a0484b97d0869d873ae58d568bdec2d26391
Kirill <litovchenko.br@gmail.com>
2023-05-06 04:25:09 +0300

    added star rating in catalog(JS)
    added function createStarRating in catalog(JS)
    
    added file stars.js with svg stars

--- src/js/catalog.js
+++ src/js/catalog.js
@@ -1,3 +1,5 @@
+import { emptyStar, fullStar, halfStar } from './stars';
+
 const axios = require('axios').default;
 getWeekMovies();
 
@@ -29,13 +31,14 @@ async function renderWeekMovies(movies) {
   for (const movie of movies) {
     const movieGenre = await getGenre(movie.id);
     const movieYear = await getYear(movie.release_date);
+    const starRating = await createStarRating(movie.vote_average);
     const hrefSprite = './images/sprite.svg#icon-star-outline';
     markup += `<li class='cards__list__item'>
       <img src='https://image.tmdb.org/t/p/w500/${movie.poster_path}' alt='${movie.title}' width='395' height='574' />
       <div>
         <h3>${movie.title}</h3>
         <p>${movieGenre} <span>${movieYear}</span></p>
-        <p>${movie.vote_average}</p>;
+        ${starRating}
       </div>
     </li>`;
   }
@@ -43,10 +46,12 @@ async function renderWeekMovies(movies) {
   movieList.innerHTML = markup;
 }
 
+// Получает год из даты
 function getYear(data) {
   return (year = data.slice(0, 4));
 }
 
+// Получает жанры фильма
 async function getGenre(movieId) {
   const API_KEY = '992758a4802a699e8df27d4d6efc34fb';
   const URL = 'https://api.themoviedb.org/3/movie/';
@@ -73,4 +78,49 @@ async function getGenre(movieId) {
   }
 }
 
+// Преобразует рейтинг в рейтинг из звезд 
+function createStarRating(data) {
+  const rating = Math.round(data);
+  console.log(rating);
+  let ratingStars = '';
 
+  switch (rating) {
+    case 0:
+      ratingStars = `${emptyStar.repeat(5)}`;
+      break;
+    case 1:
+      ratingStars = `${halfStar}${emptyStar.repeat(4)}`;
+      break;
+    case 2:
+      ratingStars = `${fullStar}${emptyStar.repeat(4)}`;
+      break;
+    case 3:
+      ratingStars = `${fullStar}${halfStar}${emptyStar.repeat(3)}`;
+      break;
+    case 4:
+      ratingStars = `${fullStar.repeat(2)}${emptyStar.repeat(3)}`;
+      break;
+    case 5:
+      ratingStars = `${fullStar.repeat(2)}${halfStar}${emptyStar.repeat(2)}`;
+      break;
+    case 6:
+      ratingStars = `${fullStar.repeat(3)}${emptyStar.repeat(2)}`;
+      break;
+    case 7:
+      ratingStars = `${fullStar.repeat(3)}${halfStar}${emptyStar}`;
+      break;
+    case 8:
+      ratingStars = `${fullStar.repeat(4)}${emptyStar}`;
+      break;
+    case 9:
+      ratingStars = `${fullStar.repeat(4)}${halfStar}`;
+      break;
+    case 10:
+      ratingStars = `${fullStar.repeat(5)}`;
+      break;
+    default:
+      throw new Error('Invalid rating');
+  }
+
+  return `<div>${ratingStars}</div>`;
+}
