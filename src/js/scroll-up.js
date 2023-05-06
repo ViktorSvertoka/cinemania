7d66394b416548c72e8fe8cc37677802ba1589f5
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-05-06 18:48:40 +0300

    Feat(JS) file scroll-up
    Added script to JS scroll-up

---
+++ src/js/scroll-up.js
@@ -0,0 +1,13 @@
+const backToTopButton = document.querySelector('#back-to-top');
+
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 100) {
+    backToTopButton.style.display = 'block';
+  } else {
+    backToTopButton.style.display = 'none';
+  }
+});
+
+backToTopButton.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
