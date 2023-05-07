0b8128af745e3bb1aca46e6d56ebe877c3b3203b
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-05-07 15:52:00 +0300

    Change links JS & HTML
    Added new links

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './js/my-library';
-
 import './js/api-service';
 import './js/mobile-menu';
 import './js/footer-modal-team';
+import './js/modal-film';
 import './js/theme-switcher';
 import './js/catalog';
 import './js/catalog-search-form';
@@ -12,10 +12,7 @@ import './js/hero-modal-oops';
 import './js/hero-modal-watch-trailer';
 import './js/hero-slider';
 import './js/loader';
-import './js/modal-film';
 import './js/upcoming-mounth';
 import './js/weekly-trends';
 import './js/genres';
 import './js/scroll-up';
-
-
