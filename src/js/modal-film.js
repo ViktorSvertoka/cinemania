c593ca18be7fcdab29d6df849f31bd5c82be528a
MariiaaIvanova <114428702+MariiaaIvanova@users.noreply.github.com>
2023-05-07 14:40:15 +0300

    modal-window_js

--- src/js/modal-film.js
+++ src/js/modal-film.js
@@ -1,73 +1,73 @@
 const API_KEY = '992758a4802a699e8df27d4d6efc34fb';
-
-const API_URL_MOVIE_DETAILS = 'https://api.themoviedb.org/3/movie';
-
-const modalFilmEl = document.querySelector('[data-modal-film]');
-const modalFilmBtn = document.querySelector('[data-modal-film-btn]');
-
-async function openModal(id) {
-    const resp = await fetch(API_URL_MOVIE_DETAILS + id, {
-        headers: {
-            "Content-Type": "application/json",
-            "X-API-KEY": API_KEY,
-        }
-    }
-    );
-    const respData = await resp.json();
+const POPUP_URL = `https://api.themoviedb.org/3/movie/`;
+const POPUP_ID =  157336;
 
 
-    modalFilmEl.classList.add('modal--show');
-    modalFilmEl.innerHTML = `
-    <div class="backdrop is-hidden" data-modal>
-<div class="modal modal-film" data-modal-film>
-    <div class="modal-film__images">
-        <img class="modal-film__img" src="https://api.themoviedb.org/3/movie" alt="${title}" width="375" height="478">
-    </div>
-<div class="modal-film__container">
-<h2 class="modal-film__title">${title}</h2>
-<div class="modal-film__thumb">
-<ul class="modal-film__list">
-    <li class="modal-film__item">
-        <p class="modal-film__text">Vote / Votes</p>
-        <p class="modal-film__vote">
-            <span class="vote">${vote}</span> / <span class="vote">${vote_count}</span>
-        </p>
-    </li>
-    <li class="modal-film__item">
-        <p class="modal-film__text">Popularity</p>
-        <p class="modal-film__pop">${populate}</p>
-    </li>
-    <li class="modal-film__item">
-        <p class="modal-film__text">Genre</p>
-        <p class="modal-film__genre">${genre}</p>
-    </li>
-</ul>
-</div>
-<h3 class="modal-film__about">About</h3>
-<p class="modal-film__text-about">${overview}</p>
-<button type="button" class="modal-film__button" data-modal-film-btn>Add to my library</button>
-    </div>
-    <!--   Svg иконка для закрытия окна  -->
-    <button class="modal-film__close" data-modal-close>
-        <!-- <svg width="24" height="24">
-            <use href="./images/icons-sprite.svg#icon-close"></use>
-        </svg> -->
-    </button>
-</div>
-    </div >
-`
-const modalFilmClose = document.querySelector('[data-modal-close]');
-modalFilmClose.addEventListener('click', () => closeModal)    
+const refs = {  
+  overlayPopUp: document.getElementById('overlayPopUp'),  
+  closeModalPopUp: document.getElementById('closeModalPopUp'),
+  openModalPopUp: document.getElementById('openModalPopUp'),
+  modalPopUp: document.getElementById('modalPopUp'),
+  btnPopUp: document.getElementById('mylibrary'),
+  closeIconPopUp: document.querySelector('.modal-film__close-icon'),
+  blokPopUp: document.querySelector('.modal-film__blok'), 
+  aboutTxtPopUp: document.querySelector('.modal-film__about-txt'),
+
+  image: document.querySelector('.modal-film__img'),
+  titles: document.querySelector('.modal-film__title'),
+  vote: document.querySelector('.vote'),
+  votes: document.querySelector('.votes'),
+  popular: document.querySelector('.popularity'),
+  genre: document.querySelector('.genres'),
+};
+
+const classes = {
+  openModal: 'open-modal',
+  visual: 'visual',
+};
+
+refs.openModalPopUp.addEventListener('click', handlePopUpModal);
+refs.closeModalPopUp.addEventListener('click', handlePopUpModal);
+refs.overlayPopUp.addEventListener('click', handlePopUpModal);
+
+document.addEventListener('keydown', handlePopUpModalClose)
+
+function handlePopUpModalClose({code}) {  
+  if (code === 'Escape' && modalPopUp.classList.contains(classes.visual)) {    
+    handlePopUpModal();
+  }
+}
+
+function handlePopUpModal() {
+  document.body.classList.toggle(classes.openModal);
+  overlayPopUp.classList.toggle(classes.visual);
+  modalPopUp.classList.toggle(classes.visual);  
+  console.log(modalPopUp);
+};
+
+
+function fetchPopUpMovies() {
+  return fetch(`${POPUP_URL}${POPUP_ID}?api_key=${API_KEY}`)
+  .then(data => {    
+    return data.json();      
+  })
+}
+
+function getPopUpMovies() {
+  fetchPopUpMovies()
+    .then(({ poster_path, title, overview, popularity, vote_average, vote_count, tagline, genres }) => {
+      console.log(genres)           
+      refs.image.src = `https://image.tmdb.org/t/p/w500/${poster_path}`;      
+      refs.titles.textContent = title;            
+      refs.vote.textContent = vote_average;         
+      refs.votes.textContent = vote_count;      
+      refs.popular.textContent = popularity;      
+      refs.genre.textContent = genres;
+      refs.aboutTxtPopUp.textContent = overview;      
+    })
+        .catch(error => console.log(error));
     
 }
 
-function closeModal() {
-   modalFilmEl.classList.remove('modal--show')
-}
-window.addEventListener("click", (e) => {
-  if (e.target === modalFilmEl) {
-    closeModal();
-  }
-})
-
+getPopUpMovies();
 
