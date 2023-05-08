1c3f0c4ce0f3be4f361ab0a4e97d6a3d82be78da
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-05-09 00:58:26 +0300

    Test

--- src/index.js
+++ src/index.js
@@ -8,6 +8,6 @@ import './js/hero';
 import './js/hero-modal-watch-trailer';
 import './js/hero-slider';
 import './js/loader';
-import './js/weekly-trends';
+// import './js/weekly-trends';
 import './js/upcoming-mounth';
 import './js/genres';
