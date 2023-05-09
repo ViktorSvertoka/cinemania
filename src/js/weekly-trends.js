f5f5c0df92ce9d27a57db0edc336cc81ebe9f0c9
Ikaruga666 <113863223+ArbalestARX7@users.noreply.github.com>
2023-05-09 20:08:22 +0300

    update weekly-trends

--- src/js/weekly-trends.js
+++ src/js/weekly-trends.js
@@ -1,10 +1,8 @@
 import APIService from './api-service-main';
 import renderMoviesCards from './cards-rendering';
+import throttle from 'lodash.throttle';
 
 const apiService = new APIService();
-let viewportSize = 0;
-
-window.addEventListener('resize', getWeeklyTrends);
 
 getWeeklyTrends();
 
@@ -16,24 +14,22 @@ async function getWeeklyTrends() {
     if (response.length === 0 || !response) {
       return error;
     }
-
-    testViewportAndDoMarkup(response, viewportSize);
+    const movies = response.slice(0, 3);
+    renderMoviesCards(movies, '.weekly-trends__list');
   } catch (error) {
     console.log(error);
   }
 }
 
-function testViewportAndDoMarkup(response, viewportSize) {
-  if (viewportSize > 768) {
-    const movies = response.slice(0, 3);
-    renderMoviesCards(movies);
-  } else {
-    const movies = response.slice(0, 1);
-    renderMoviesCards(movies);
-  }
-}
+window.addEventListener('resize', throttle(cardListShuffling, 100));
 
-async function updateViewport() {
-  console.log(viewportSize);
-  return (viewportSize = window.innerWidth);
+function cardListShuffling() {
+  const cardList = document.querySelectorAll('.cards__list__item');
+  if (window.innerWidth < 768) {
+    cardList[1].classList.add('weekly-trends__none');
+    cardList[2].classList.add('weekly-trends__none');
+  } else {
+    cardList[1].classList.remove('weekly-trends__none');
+    cardList[2].classList.remove('weekly-trends__none');
+  }
 }
