f0a6127a7aeefc049dbf87cbbff9d0303f1ff60c
IhorKulinskyi <110032104+IhorKulinskyi@users.noreply.github.com>
2023-05-10 01:37:16 +0300

    oops message

--- src/js/my-library.js
+++ src/js/my-library.js
@@ -1,8 +1,22 @@
-import renderMoviesCards from "./cards-rendering";
-import LibraryAPI from "./library-api-service";
+import renderMoviesCards from './cards-rendering';
+import LibraryAPI from './library-api-service';
 
 const library = new LibraryAPI();
 
 library.setLibrary();
 
-renderMoviesCards(library.movies,'.my-lib__gallery-list');
+checkStorage();
+
+function checkStorage() {
+  if (library.movies.length === 0) {
+    console.log(library.movies.length);
+    if (!document.querySelector('.my-lib__no-movies')) {
+      return;
+    }
+    document
+      .querySelector('.my-lib__no-movies')
+      .classList.remove('visually-hidden');
+  } else {
+    renderMoviesCards(library.movies, '.my-lib__gallery-list');
+  }
+}
