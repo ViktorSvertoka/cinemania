cfc2139849f7cd3b7c587e492a4e85646a5595f7
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-05-09 13:44:30 +0300

    Test

--- src/js/modal-film.js
+++ src/js/modal-film.js
@@ -1,16 +1,17 @@
+// import renderMoviesCards from './cards-rendering.js';
+
 const API_KEY = '992758a4802a699e8df27d4d6efc34fb';
 const BASE_URL = `https://api.themoviedb.org/3/movie/`;
-const MOVIE_ID =  157336;
+const MOVIE_ID = 157336;
 
-
-const refs = {  
-  overlayPopUp: document.getElementById('overlayPopUp'),  
+const refs = {
+  overlayPopUp: document.getElementById('overlayPopUp'),
   closeModalPopUp: document.getElementById('closeModalPopUp'),
   // openModalPopUp: document.getElementById('openModalPopUp'),
   modalPopUp: document.getElementById('modalPopUp'),
   btnPopUp: document.getElementById('mylibrary'),
   closeIconPopUp: document.querySelector('.modal-film__close-icon'),
-  blokPopUp: document.querySelector('.modal-film__blok'), 
+  blokPopUp: document.querySelector('.modal-film__blok'),
   aboutTxtPopUp: document.querySelector('.modal-film__about-txt'),
 
   image: document.querySelector('.modal-film__img'),
@@ -30,10 +31,10 @@ const classes = {
 refs.closeModalPopUp.addEventListener('click', handlePopUpModal);
 refs.overlayPopUp.addEventListener('click', handlePopUpModal);
 
-document.addEventListener('keydown', handlePopUpModalClose)
+document.addEventListener('keydown', handlePopUpModalClose);
 
-function handlePopUpModalClose({code}) {  
-  if (code === 'Escape' && modalPopUp.classList.contains(classes.visual)) {    
+function handlePopUpModalClose({ code }) {
+  if (code === 'Escape' && modalPopUp.classList.contains(classes.visual)) {
     handlePopUpModal();
   }
 }
@@ -42,31 +43,36 @@ function handlePopUpModal() {
   getPopUpMovies();
   document.body.classList.toggle(classes.openModal);
   overlayPopUp.classList.toggle(classes.visual);
-  modalPopUp.classList.toggle(classes.visual);  
+  modalPopUp.classList.toggle(classes.visual);
   console.log(modalPopUp);
-};
-
+}
 
 function fetchPopUpMovies() {
-  return fetch(`${BASE_URL}${MOVIE_ID}?api_key=${API_KEY}`)
-  .then(data => {    
-    return data.json();      
-  })
+  return fetch(`${BASE_URL}${MOVIE_ID}?api_key=${API_KEY}`).then(data => {
+    return data.json();
+  });
 }
 
 async function getPopUpMovies() {
   try {
-    const { poster_path, title, overview, popularity, vote_average, vote_count, genres } = await fetchPopUpMovies();    
-    refs.image.src = `https://image.tmdb.org/t/p/w500/${poster_path}`;      
-    refs.titles.textContent = title;            
-    refs.vote.textContent = vote_average;         
-    refs.votes.textContent = vote_count;      
-    refs.popular.textContent = popularity;    
-    console.log(genres);       
-    refs.genre.textContent = genres.map((genres) => genres.name).join(" ");   
-    refs.aboutTxtPopUp.textContent = overview;      
+    const {
+      poster_path,
+      title,
+      overview,
+      popularity,
+      vote_average,
+      vote_count,
+      genres,
+    } = await fetchPopUpMovies();
+    refs.image.src = `https://image.tmdb.org/t/p/w500/${poster_path}`;
+    refs.titles.textContent = title;
+    refs.vote.textContent = vote_average;
+    refs.votes.textContent = vote_count;
+    refs.popular.textContent = popularity;
+    console.log(genres);
+    refs.genre.textContent = genres.map(genres => genres.name).join(' ');
+    refs.aboutTxtPopUp.textContent = overview;
   } catch (error) {
     console.log(error);
   }
 }
-
