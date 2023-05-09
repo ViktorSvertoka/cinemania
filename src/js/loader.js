563ac5a20e4e7a3d8b048900a02decd84aefcb0e
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2023-05-09 21:11:30 +0200

    Fix

---
+++ src/js/loader.js
@@ -0,0 +1,9 @@
+function loaderClassAdd() {
+  document.getElementById('loader').classList.add('is-active');
+}
+
+function loaderClassRemove() {
+  document.getElementById('loader').classList.remove('is-active');
+}
+
+export { loaderClassAdd, loaderClassRemove };
