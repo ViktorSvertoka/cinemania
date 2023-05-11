29fc5a261f1b8635b22065c244c6cbb31dfa9a64
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2023-05-11 07:35:02 +0200

    New Loader

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,35 +1,29 @@
-let loader;
-let overlay;
-
 function createLoaderElements() {
-  overlay = document.createElement('div');
-  overlay.className = 'loader__overlay';
-
-  loader = document.createElement('div');
-  loader.className = 'loader';
-
-  document.body.appendChild(overlay);
-  document.body.appendChild(loader);
+  // overlay = document.createElement('div');
+  // overlay.className = 'loader__overlay';
+  // loader = document.createElement('div');
+  // loader.className = 'loader';
+  // document.body.appendChild(overlay);
+  // document.body.appendChild(loader);
 }
 
 function loaderShow() {
-  if (!loader || !overlay) {
-    createLoaderElements();
-  }
-
-  overlay.classList.add('is-active');
-  loader.classList.add('is-active');
+  // if (!loader || !overlay) {
+  //   createLoaderElements();
+  // }
+  // overlay.classList.add('is-active');
+  document.querySelector('.wrap-spinner').classList.add('is-active');
 }
 
 function loaderHide() {
-  if (loader && overlay) {
-    overlay.classList.remove('is-active');
-    loader.classList.remove('is-active');
-    setTimeout(() => {
-      overlay.remove();
-      loader.remove();
-    }, 500);
-  }
+  // if (loader && overlay) {
+  //   overlay.classList.remove('is-active');
+  document.querySelector('.wrap-spinner').classList.remove('is-active');
+  //   setTimeout(() => {
+  //     overlay.remove();
+  //     loader.remove();
+  //   }, 500);
+  // }
 }
 
 export { loaderShow, loaderHide };
