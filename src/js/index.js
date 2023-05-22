97a1a4d146bc904a174ea56b53243949a70f1236
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-05-22 19:22:15 +0300

    Perf(JS & HTML) Update files

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+import './loader';
+import './my-library';
+import './theme-switcher';
+import './hero';
+import './hero-slider';
+import './hero-modal-watch-trailer';
+import './mobile-menu';
+import './upcoming-month';
+import './scroll-up';
+import './footer-modal-team';
+import './modal-film';
